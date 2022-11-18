17b2f5d35219ec3818ae200d9101e74cbc9615c9
Antonio Jorge Garcia Tranin <63371272+JorgeTranin@users.noreply.github.com>
2022-11-18 17:48:06 -0400

    Implementação do Banco de Dados

--- app/src/main/java/com/example/visitacaoamvcm/CadastroUsuario.kt
+++ app/src/main/java/com/example/visitacaoamvcm/CadastroUsuario.kt
@@ -2,10 +2,38 @@ package com.example.visitacaoamvcm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.visitacaoamvcm.databinding.ActivityCadastroUsuarioBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class CadastroUsuario : AppCompatActivity() {
+    private val db = FirebaseFirestore.getInstance()
+    private lateinit var binding: ActivityCadastroUsuarioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastro_usuario)
+        binding = ActivityCadastroUsuarioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnSalvarCadastro.setOnClickListener {
+            val NomeVisitante = binding.editTextNomeCompleto.text.toString()
+            val Documento = binding.editTextDocumento.text.toString()
+            val dataDeNascimento = binding.editTextDatadeNascimento.text.toString()
+            val Endereco = binding.editTextEndereOAtual.text.toString()
+
+
+            salvarNoticia(NomeVisitante, Documento, dataDeNascimento, Endereco)
+        }
+
+
+
+    }
+
+    private fun salvarNoticia(NomeVisitante:String,Documento:String, dataDeNascimento:String, Endereco:String) {
+        val mapVisitantes = hashMapOf(
+            "NomeVisitante" to NomeVisitante,
+            "Documento" to Documento,
+            "dataDeNascimento" to dataDeNascimento,
+            "Endereco" to Endereco
+        )
+        db.collection("Visitantes").document("Visitante").set()
     }
 }
