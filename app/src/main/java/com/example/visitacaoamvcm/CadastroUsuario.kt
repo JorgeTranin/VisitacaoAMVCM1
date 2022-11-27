05cfc6f88ffe3dc4c7c271c0d351d1a799a47a0e
jorgetranin <antoniotraninjorge@gmail.com>
2022-11-27 17:26:19 -0400

    Implemmentação seleção de imagem do visitante e apresentação no meu btn_ImagemVisitante

--- app/src/main/java/com/example/visitacaoamvcm/CadastroUsuario.kt
+++ app/src/main/java/com/example/visitacaoamvcm/CadastroUsuario.kt
@@ -1,16 +1,63 @@
 package com.example.visitacaoamvcm
 
 //import Business.mCategoriadeVisitantes
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.visitacaoamvcm.Business.mCategoriadeVisitantes
 import com.example.visitacaoamvcm.databinding.ActivityCadastroUsuarioBinding
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_cadastro_usuario.*
 
 class CadastroUsuario : AppCompatActivity() {
+    lateinit var dialog: AlertDialog
 
+    companion object {
+        private val PERMISSAO_GALERIA = Manifest.permission.READ_EXTERNAL_STORAGE
+    }
+
+    private val requestGaleria =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { permissao ->
+            if (permissao) {
+                resultGaleria.launch(
+                    Intent(
+                        Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    )
+                )
+            } else {
+                showDialogPermissao()
+            }
+        }
+    private val resultGaleria =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.data?.data != null) {
+                val bitmap: Bitmap = if (Build.VERSION.SDK_INT < 28) {
+                    MediaStore.Images.Media.getBitmap(
+                        baseContext.contentResolver,
+                        result.data?.data
+                    )
+                } else {
+                    val source =
+                        ImageDecoder.createSource(this.contentResolver, result.data?.data!!)
+                    ImageDecoder.decodeBitmap(source)
+
+                }
+                binding.btnImagemVisitante.setImageBitmap(bitmap)
+            }
+
+        }
     private val db = FirebaseFirestore.getInstance()
     private lateinit var binding: ActivityCadastroUsuarioBinding
     var categoria: mCategoriadeVisitantes? = null
@@ -20,6 +67,9 @@ class CadastroUsuario : AppCompatActivity() {
         binding = ActivityCadastroUsuarioBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnImagemVisitante.setOnClickListener {
+            verificaPermissaoDaGaleria()
+        }
 
         // Pegando as informações passadas pela tela de Pesquisa de Visitantes e passando para a variavel categoria
         categoria = intent.getParcelableExtra("categoriaNome")
@@ -76,14 +126,71 @@ class CadastroUsuario : AppCompatActivity() {
                 }
 
             }.addOnFailureListener {
-            Toast.makeText(this, "Erro ao Cadastrar", Toast.LENGTH_LONG).show()
-        }
+                Toast.makeText(this, "Erro ao Cadastrar", Toast.LENGTH_LONG).show()
+            }
     }
+
     //Função para limpar os campos quando for Salvo no DB
-    private fun limparCampos(){
+    private fun limparCampos() {
         binding.editTextDocumento.setText("")
         binding.editTextEndereOAtual.setText("")
         binding.editTextDatadeNascimento.setText("")
         binding.editTextNomeCompleto.setText("")
     }
+
+    //-----------------------------------------------------------------Metodos para buscar imagem na galeria e exibir--------------------------------------------------------------------------
+
+
+    //Verifica se o tem permissao para acessar a galeria, da camera da localização entre outras, RETORNA TRUE OU FALSE
+    private fun verificaPermissao(permissao: String) =
+        //verificando se esta permissao esta aceita ou não
+        ContextCompat.checkSelfPermission(this, permissao) == PackageManager.PERMISSION_GRANTED
+
+
+    //Verifica se a permissao para acessar a galeria foi aceita
+    private fun verificaPermissaoDaGaleria() {
+        val permissaoGaleriaAceita = verificaPermissao(CadastroUsuario.PERMISSAO_GALERIA)
+
+        when {
+            // se esta aceita a permissao irá abrir a galeria
+            permissaoGaleriaAceita -> {
+                resultGaleria.launch(
+                    Intent(
+                        Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    )
+                )
+            }
+            shouldShowRequestPermissionRationale(CadastroUsuario.PERMISSAO_GALERIA) -> showDialogPermissao()
+
+
+            else -> requestGaleria.launch(CadastroUsuario.PERMISSAO_GALERIA)
+
+
+        }
+
+    }
+
+    private fun showDialogPermissao() {
+        val buider = AlertDialog.Builder(this).setTitle("Atenção")
+            .setMessage("1 Precisamos do acesso a galeria do dispositivo, deseja pernitir agora ?")
+            .setNegativeButton("Não") { _, _ ->
+                dialog.dismiss()
+            }
+            .setPositiveButton("Sim") { _, _ ->
+                val intent = Intent(
+                    android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.fromParts("package", packageName, null)
+                )
+
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                dialog.dismiss()
+            }
+
+        dialog = buider.create()
+        dialog.show()
+
+    }
+
 }
--- app/src/main/java/com/example/visitacaoamvcm/teste.kt
+++ app/src/main/java/com/example/visitacaoamvcm/teste.kt
@@ -1,13 +1,53 @@
 package com.example.visitacaoamvcm
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.visitacaoamvcm.Business.mCategoriadeVisitantes
 import com.example.visitacaoamvcm.databinding.ActivityTesteBinding
-import kotlinx.android.synthetic.main.activity_teste.*
 
 class teste : AppCompatActivity() {
+    lateinit var dialog: AlertDialog
+
+    companion object {
+        private val PERMISSAO_GALERIA = Manifest.permission.READ_EXTERNAL_STORAGE
+    }
+
+    private val requestGaleria =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { permissao ->
+            if (permissao) {
+                resultGaleria.launch(
+                    Intent(
+                        Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    )
+                )
+            } else {
+                showDialogPermissao()
+            }
+        }
+    private val resultGaleria =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val bitmap: Bitmap = if (Build.VERSION.SDK_INT < 28) {
+                MediaStore.Images.Media.getBitmap(baseContext.contentResolver, result.data?.data)
+            } else {
+                val source = ImageDecoder.createSource(this.contentResolver, result.data?.data!!)
+                ImageDecoder.decodeBitmap(source)
+
+            }
+            binding.imageButtonSelecionar.setImageBitmap(bitmap)
+
+        }
     lateinit var binding: ActivityTesteBinding
     var categoria: mCategoriadeVisitantes? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,11 +56,74 @@ class teste : AppCompatActivity() {
         setContentView(binding.root)
 
 
+        binding.imageButtonSelecionar.setOnClickListener {
+
+        }
+
+        binding.btnSalvarImagem.setOnClickListener {
+            verificaPermissaoDaGaleria()
+        }
+
+
         supportActionBar?.title = categoria?.nome
         categoria = intent.getParcelableExtra("categoriaNome")
 
 
-        Toast.makeText(this, categoria?.nome.toString(), Toast.LENGTH_LONG).show()
-        textView2.text = categoria?.nome.toString()
+        //Toast.makeText(this, categoria?.nome.toString(), Toast.LENGTH_LONG).show()
+        //textView2.text = categoria?.nome.toString()
     }
+
+
+    //Verifica se o tem permissao para acessar a galeria, da camera da localização entre outras, RETORNA TRUE OU FALSE
+    private fun verificaPermissao(permissao: String) =
+        //verificando se esta permissao esta aceita ou não
+        ContextCompat.checkSelfPermission(this, permissao) == PackageManager.PERMISSION_GRANTED
+
+
+    //Verifica se a permissao para acessar a galeria foi aceita
+    private fun verificaPermissaoDaGaleria() {
+        val permissaoGaleriaAceita = verificaPermissao(PERMISSAO_GALERIA)
+
+        when {
+            // se esta aceita a permissao irá abrir a galeria
+            permissaoGaleriaAceita -> {
+                resultGaleria.launch(
+                    Intent(
+                        Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    )
+                )
+            }
+            shouldShowRequestPermissionRationale(PERMISSAO_GALERIA) -> showDialogPermissao()
+
+
+            else -> requestGaleria.launch(PERMISSAO_GALERIA)
+
+
+        }
+
+    }
+
+    private fun showDialogPermissao() {
+        val buider = AlertDialog.Builder(this).setTitle("Atenção")
+            .setMessage("1 Precisamos do acesso a galeria do dispositivo, deseja pernitir agora ?")
+            .setNegativeButton("Não") { _, _ ->
+                dialog.dismiss()
+            }
+            .setPositiveButton("Sim") { _, _ ->
+                val intent = Intent(
+                    android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.fromParts("package", packageName, null)
+                )
+
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                dialog.dismiss()
+            }
+
+        dialog = buider.create()
+        dialog.show()
+
+    }
+
 }
